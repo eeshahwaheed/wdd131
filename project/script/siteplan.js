b7d5385c2cbab742e4e5b4844d054ee1557d386c
eeshahwaheed <eeshah.waheednazeer@outlook.com>
2024-10-19 18:49:45 +0500

    ok

---
+++ project/script/siteplan.js
@@ -0,0 +1,9 @@
+const copyrightYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+
+const today = new Date();
+copyrightYear.textContent = today.getFullYear(); 
+
+
+lastModified.textContent = "Last Modified: " + document.lastModified; 
